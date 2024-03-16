1411fd8e36fe8004dec22d7b8bc0f45ad42c7998
Aki Rehn <aki@rehn.fi>
2024-03-16 11:41:19 +0200

    Login automatically using cookies.
    - Require login only once by implementing persistent cookie storage.

--- Application.js
+++ Application.js
@@ -3,14 +3,16 @@ import GLib from 'gi://GLib';
 import Gio from 'gi://Gio';
 import WebKit from 'gi://WebKit';
 
+const APPLICATION_ID = 'dev.rehn.quick-chatgpt'
+
 class Application {
     constructor() {
-        this.application = new Gtk.Application({
-            application_id: 'dev.rehn.GnomeChatGPT',
+        this.app = new Gtk.Application({
+            application_id: APPLICATION_ID,
             flags: Gio.ApplicationFlags.FLAGS_NONE,
         });
 
-        this.application.connect('activate', this._onActivate.bind(this));
+        this.app.connect('activate', this._onActivate.bind(this));
     }
 
     // Application activation callback
@@ -21,17 +23,27 @@ class Application {
     // Create the main application window
     _createWindow() {
         let window = new Gtk.ApplicationWindow({
-            application: this.application,
-            title: "ChatGPT",
+            application: this.app,
+            title: 'ChatGPT',
             default_height: 600,
-            default_width: 800,
+            default_width: 370,
+            decorated: false,
         });
 
+        const appId = this.app.get_application_id();
+        const appCacheDir = GLib.build_filenamev([GLib.get_user_cache_dir(), appId]);
+        GLib.mkdir_with_parents(appCacheDir, 0o775)
+
         // Create a new WebKit WebView
-        let webView = new WebKit.WebView();
+        const webView = new WebKit.WebView();
+        const networkSession = webView.get_network_session();
+
+        const cookieManager = networkSession.get_cookie_manager();
+        const cookiesFilename = GLib.build_filenamev([appCacheDir, 'cookies.sqlite']);
+        cookieManager.set_persistent_storage(cookiesFilename, WebKit.CookiePersistentStorage.SQLITE);
 
         // Load a web page into the WebView
-        webView.load_uri("https://chat.openai.com/chat");
+        webView.load_uri('https://chat.openai.com/chat');
 
         // Add the WebView to the window
         window.set_child(webView);
@@ -42,13 +54,12 @@ class Application {
         // Error handling for WebView
         webView.connect('load-failed', (webView, load_event, url, error) => {
             console.error(`Failed to load URL ${url}: ${error.message}`);
-            // Optionally, display an error message to the user or take other appropriate actions
         });
     }
 
     // Run the application
     run(argv = []) {
-        this.application.run(argv);
+        this.app.run(argv);
     }
 }
 
